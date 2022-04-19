3c4cba77c6739ae3ffd64caa31e557022b690e62
GeonhyoPark815 <rjsgy0815@gmail.com>
2022-04-20 01:17:28 +0900

    Feat: Change dailyMyPostWidget & dailyOtherPostWidget to build with pageview

--- lib/ui/view/screen/post/post_main_screen.dart
+++ lib/ui/view/screen/post/post_main_screen.dart
@@ -1,33 +1,48 @@
 import 'package:blur/blur.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:couple_seflie_app/data/model/daily_couple_post_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-bool isMePostUploaded = false; // temp
+import '../../../../data/model/post_model.dart';
+import '../../widget/post/post_daily_info_widget.dart';
+import '../../widget/post/post_appbar_widget.dart';
+
+bool isMyPostUploaded = true; // temp
 bool isOtherPostUploaded = true; // temp
+List<DailyCouplePostModel> dailyPosts = [];
 
 class PostMainScreen extends StatelessWidget {
   const PostMainScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    FocusScope.of(context).unfocus();
     return Scaffold(
-      appBar: AppBar(
-        toolbarHeight: 0,
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-      ),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            mainAppBar(),
-            dailyInfoWidget(),
-            dailyMePostWidget(),
-            Container(
-              height: 30.w,
-            ),
-            dailyOtherPostWidget(),
-          ]
+      body: SafeArea(
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              postAppBarWidget("/mainScreen", context),
+              postDailyInfoWidget("/mainScreen", context),
+              PageView.builder(
+                scrollDirection: Axis.horizontal,
+                controller: PageController(initialPage: dailyPosts.length),
+                itemCount: dailyPosts.length,
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: <Widget>[
+                      dailyMyPostWidget(context),
+                      Container(
+                        height: 30.w,
+                      ),
+                      dailyOtherPostWidget(context),
+                    ],
+                  );
+                }
+              )
+            ]
+          ),
         ),
       ),
     );
@@ -36,161 +51,8 @@ class PostMainScreen extends StatelessWidget {
   /// 화면 분할 비율
   /// 295 : 1 : 94
 
-  /// Main Appbar
-  /// 기존 앱바는 커스텀이 어려워 Widget으로 만들어 사용
-  Widget mainAppBar() {
-    return Container(
-      height: 90.w,
-      padding: EdgeInsets.only(left: 20.w),
-      child: Row(
-        children: <Widget>[
-          Container(
-            width: 275.w,
-            alignment: Alignment.centerLeft,
-            child: IconButton(
-              padding: EdgeInsets.only(left: 0),
-              constraints: BoxConstraints(),
-              icon: Icon(
-                Icons.menu,
-                color: Color(0xFF667080),
-                size: 24.w,
-              ),
-              onPressed: () {  },
-            ),
-          ),
-          Container(
-            color: Color(0xFF000000),
-            width: 1.w,
-          ),
-          Container(
-            width: 94.w,
-          ),
-        ],
-      ),
-    );
-  }
-
-  /// 날짜, 제시 질문, 이모티콘 등의 상단 위젯
-  Widget dailyInfoWidget() {
-    return Container(
-      width: 390.w,
-      height: 190.w,
-      padding: EdgeInsets.only(left: 20.w),
-      child: Row(
-        children: <Widget>[
-          Container(
-            width: 275.w,
-            margin: EdgeInsets.only(bottom: 30.w),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                /// 날짜 정보
-                Container(
-                  width: 48.w,
-                  height: 94.w,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      /// year
-                      Container(
-                        width: 48.w,
-                        height: 24.w,
-                        color: Color(0xFF000000),
-                        alignment: Alignment.center,
-                        child: Text(
-                          // temp
-                          "2022",
-                          style: TextStyle(
-                              color: Color(0xFFFFFFFF),
-                              fontWeight: FontWeight.w300,
-                              fontSize: 18.w,
-                              fontStyle: FontStyle.normal,
-                              //fontFamily:
-                              //height: 24.w
-                          ),
-                        ),
-                      ),
-                      /// month
-                      Container(
-                        alignment: Alignment.center,
-                        height: 20.w,
-                        child: Text(
-                          // temp
-                          "March",
-                          style: TextStyle(
-                              color: Color(0xFF000000),
-                              fontWeight: FontWeight.w600,
-                              fontSize: 12.w,
-                              fontStyle: FontStyle.normal,
-                              //height: 12.w
-                              //fontFamily:
-                          ),
-                        ),
-                      ),
-                      /// day
-                      Container(
-                        height: 45.w,
-                        alignment: Alignment.center,
-                        child: Text(
-                          // temp
-                          "27",
-                          style: TextStyle(
-                              color: Color(0xFF000000),
-                              fontWeight: FontWeight.w600,
-                              fontSize: 30.w,
-                              fontStyle: FontStyle.normal,
-                              //height: 45.w
-                              //fontFamily:
-                          ),
-                        ),
-                        decoration: BoxDecoration(
-                          border: Border(
-                            top: BorderSide(
-                                width: 1.w,
-                                color: Color(0xFF000000)),
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                /// 해당 날짜의 질문
-                Container(
-                  height: 64.w,
-                  child: Text(
-                    // temp
-                    "상대방을 생각하면 \n지어지는 표정",
-                    style: TextStyle(
-                      color: Color(0xFF000000),
-                      fontWeight: FontWeight.w400,
-                      fontSize: 26.w,
-                      fontStyle: FontStyle.normal,
-                      //fontFamily:
-                    ),
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 2,
-                  ),
-                )
-              ],
-            ),
-          ),
-          Container(
-            color: Color(0xFF000000),
-            width: 1.w,
-          ),
-          Container(
-            width: 94.w,
-
-          ),
-        ],
-      ),
-    );
-  }
-
   /// My Daily Post
-  Widget dailyMePostWidget() {
+  Widget dailyMyPostWidget(BuildContext context) {
     return Container(
       height: 220.w,
       decoration: BoxDecoration(
@@ -206,10 +68,13 @@ class PostMainScreen extends StatelessWidget {
         ),
       ),
       alignment: Alignment.center,
-      child: isMePostUploaded == false ?
-      /// 업로드 링크 연결
+      child: isMyPostUploaded == false ?
+      /// 업로드 페이지
       InkWell(
-        onTap: (){},
+        onTap: (){
+          FocusScope.of(context).unfocus();
+          Navigator.pushNamed(context, '/postEditScreen');
+        },
         child: Container(
           height: 80.w,
           width: 120.w,
@@ -251,7 +116,10 @@ class PostMainScreen extends StatelessWidget {
       Row(
         children: <Widget>[
           InkWell(
-            onTap: (){},
+            onTap: (){
+              // temp
+              Navigator.pushNamed(context, "/postDetailScreen");
+            },
             child: Container(
               // temp - 사진 비율 4:3으로 임의 설정
               width: 295.w,
@@ -283,7 +151,7 @@ class PostMainScreen extends StatelessWidget {
   }
 
   /// Partner's Daily Post
-  Widget dailyOtherPostWidget() {
+  Widget dailyOtherPostWidget(BuildContext context) {
     return Container(
       height: 220.w,
       decoration: BoxDecoration(
@@ -299,7 +167,7 @@ class PostMainScreen extends StatelessWidget {
         ),
       ),
       alignment: Alignment.center,
-      child: isMePostUploaded == false ?
+      child: isMyPostUploaded == false ?
       Container(
         child: isOtherPostUploaded == false ?
         /// 상대 답변 기다리는 중 안내
@@ -443,14 +311,17 @@ class PostMainScreen extends StatelessWidget {
               width: 95.w,
             ),
             InkWell(
-              onTap: (){},
+              onTap: (){
+                // temp
+                Navigator.pushNamed(context, "/postDetailScreen");
+              },
               child: Container(
                 // temp - 사진 비율 4:3으로 임의 설정
                 width: 295.w,
                 height: 295.w * 3 / 4,
                 child: CachedNetworkImage(
                   // temp
-                  imageUrl: "http://file3.instiz.net/data/cached_img/upload/2021/07/16/3/7cede7a98eaff4c6047fedea6c6bca6d.jpg",
+                  imageUrl: "https://file3.instiz.net/data/cached_img/upload/2021/07/16/3/7cede7a98eaff4c6047fedea6c6bca6d.jpg",
                   width: 295.w,
                   height: 295.w * 3 / 4,
                   progressIndicatorBuilder: (context, url, downloadProgress) =>
