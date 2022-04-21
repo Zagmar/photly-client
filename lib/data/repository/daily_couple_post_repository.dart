94c979ea4b84f1295865d183da4390386d264bfa
GeonhyoPark815 <rjsgy0815@gmail.com>
2022-04-22 02:12:32 +0900

    Feat: Add functions to get reponse about dailyCouplePosts via api

--- lib/data/repository/daily_couple_post_repository.dart
+++ lib/data/repository/daily_couple_post_repository.dart
@@ -1,15 +1,42 @@
+import 'package:couple_seflie_app/data/model/daily_couple_post_model.dart';
+import 'package:couple_seflie_app/data/model/post_model.dart';
+import 'package:couple_seflie_app/ui/view_model/daily_couple_post_view_model.dart';
+import 'package:intl/intl.dart';
+
 import '../datasource/local_datasource.dart';
-import '../datasource/remote_datasourcee.dart';
+import '../datasource/remote_datasource.dart';
 
 class DailyCouplePostRepository {
   final RemoteDataSource _remoteDataSource = RemoteDataSource();
   final LocalDataSource _localDataSource = LocalDataSource();
-  static const String DAILY_POST_LIST = "https://jsonplaceholder.typicode.com/users";
+  static const String DAILY_POST_LIST = "$PHOTLY/user";
 
-  /// input : nPosts, date
-  /// output : postImageUrl, postUserId, postIsPublic, postEditTime, postText, postEmotion, postWeather, postLocation
-  /// PostList 불러오기
-  Future<Object> getDailyPosts(DateTime lastPostDate, int nPosts) async {
-    return await _remoteDataSource.getFromUri(DAILY_POST_LIST);
+  /// Get dailyCouplePosts
+  // input : userId, lastPostDate, nPosts
+  // output : dailyPostDate, questionType, questionText, questionImageUrl, userPostId, userPostImageUrl, partnerPostId, partnerPostImageUrl
+  Future<Object> getDailyCouplePosts(String userId, String lastPostDate, int nPosts) async {
+    // convert inputData to use for API
+    Map<String, String> inputData = {
+      'userId' : userId,
+      'postDate' : lastPostDate,
+      'nPosts' : nPosts.toString(),
+    };
+
+    // call API
+    return await _remoteDataSource.getFromUri(DAILY_POST_LIST, inputData);
+  }
+
+
+  /// Create today's new couplePost
+  // input : userId
+  Future<Object> createDailyCouplePost(String userId) async {
+    // convert inputData to use for API
+    Map<String, String> inputData = {
+      'userId' : userId,
+      'postDate' : DateFormat('yyyyMMdd').format(DateTime.now()).toString(),
+    };
+
+    // call API
+    return await _remoteDataSource.postToUri(DAILY_POST_LIST, inputData);
   }
 }
