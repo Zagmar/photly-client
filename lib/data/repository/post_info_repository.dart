c34c57b5ff4505ddec63ec3ac295d710ca5df9f4
GeonhyoPark815 <rjsgy0815@gmail.com>
2022-04-22 02:13:06 +0900

    Feat: Add functions to get reponse about post via api

--- lib/data/repository/post_info_repository.dart
+++ lib/data/repository/post_info_repository.dart
@@ -1,34 +1,62 @@
 import 'package:couple_seflie_app/data/model/post_model.dart';
 
 import '../datasource/local_datasource.dart';
-import '../datasource/remote_datasourcee.dart';
+import '../datasource/remote_datasource.dart';
 
 class PostInfoRepository {
   final RemoteDataSource _remoteDataSource = RemoteDataSource();
   final LocalDataSource _localDataSource = LocalDataSource();
-  static const String POST = "https://jsonplaceholder.typicode.com/users";
+  static const String POST = "$PHOTLY/post";
 
+  /// Get post
+  // input : postId
+  // output : postModel
+  Future<Object> getPost(int postId) async {
+    // convert inputData to use for API
+    Map<String, String> inputData = {
+      'postId' : postId.toString(),
+    };
 
-
-  /// input : postId
-  /// output : postImageUrl, postUserId, postIsPublic, postEditTime, postText, postEmotion, postWeather, postLocation
-  Future<Object> getPost(String postId) async {
-    return await _remoteDataSource.getFromUri(POST);
+    // call API
+    return await _remoteDataSource.getFromUri(POST, inputData);
   }
 
-  newPost(PostModel newPost) {
-    return _remoteDataSource.postToUri(POST,);
+  /// Create post
+  // input : postModel
+  Future<Object> createPost(PostModel postModel) async {
+    // convert inputData to use for API
+    Map<String, String> inputData = {
+      'postId' : postModel.postId.toString(),
+      'postUserId' : postModel.postUserId,
+      'postImageUrl' : postModel.postImageUrl,
+      'postText' : postModel.postText.toString(),
+      'postEmotion' : postModel.postEmotion.toString(),
+      'postLocation' : postModel.postLocation.toString(),
+      'postEditTime' : postModel.postEditTime.toString(),
+      'postIsPublic' : postModel.postIsPublic.toString(),
+      'postWeather' : postModel.postWeather.toString(),
+    };
+
+    // call API
+    return await _remoteDataSource.postToUri(POST, inputData);
   }
 
-  editPost(PostModel editedPost) {
-    return _remoteDataSource.putToUri(POST);
-  }
+  /// Edit post
+  // input : postModel
+  Future<Object> editPost(PostModel postModel) async {
+    // convert inputData to use for API
+    Map<String, String> inputData = {
+      'postId' : postModel.postId.toString(),
+      'postImageUrl' : postModel.postImageUrl,
+      'postText' : postModel.postText.toString(),
+      'postEmotion' : postModel.postEmotion.toString(),
+      'postLocation' : postModel.postLocation.toString(),
+      'postEditTime' : postModel.postEditTime.toString(),
+      'postIsPublic' : postModel.postIsPublic.toString(),
+      'postWeather' : postModel.postWeather.toString(),
+    };
 
-  deletePost() {
-    return _remoteDataSource.deleteFromUri(POST);
-  }
-
-  changeIsPublic() {
-    return _remoteDataSource.putToUri(POST);
+    // call API
+    return await _remoteDataSource.putToUri(POST, inputData);
   }
 }
