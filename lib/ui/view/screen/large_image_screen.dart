29692f1ebfdc6718f799b8600844c7d9b7f9d8c5
GeonhyoPark815 <rjsgy0815@gmail.com>
2022-04-28 17:59:39 +0900

    Feat: Add screen to show image as full size

---
+++ lib/ui/view/screen/large_image_screen.dart
@@ -0,0 +1,45 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../view_model/post_view_model.dart';
+
+class LargeImageScreen extends StatelessWidget {
+  LargeImageScreen({Key? key}) : super(key: key);
+  late PostViewModel _postViewModel;
+
+  @override
+  Widget build(BuildContext context) {
+    _postViewModel = Provider.of<PostViewModel>(context);
+    return ChangeNotifierProvider(
+      create: (_) => PostViewModel(),
+      child: largeImageScreen(context),
+    );
+  }
+
+  Widget largeImageScreen(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(0xFF000000),
+          leading: Container(),
+          actions: <Widget>[
+            IconButton(
+                onPressed: (){
+                  Navigator.of(context).pop();
+                },
+                icon: Icon(Icons.clear)
+            )
+          ],
+          elevation: 0,
+        ),
+        body: Container(
+          color: Color(0xFF000000),
+          alignment: Alignment.center,
+          child: CachedNetworkImage(
+            imageUrl: _postViewModel.tempImageUrl,
+            fit: BoxFit.fitWidth,
+          ),
+        )
+    );
+  }
+}
