a88f3ec765b8ebc25709d595278b23651312aefb
GeonhyoPark815 <rjsgy0815@gmail.com>
2022-04-30 09:13:27 +0900

    Fix: Fix button ui issue to perfect screens

--- lib/ui/view/screen/login/find_id_screen.dart
+++ lib/ui/view/screen/login/find_id_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 
 import '../../../view_model/login_view_model.dart';
@@ -11,7 +12,7 @@ class FindIdScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     _context = context;
-    _loginViewModel = Provider.of<LoginViewModel>(context);
+    _loginViewModel = Provider.of<LoginViewModel>(_context);
     return ChangeNotifierProvider(
       create: (_) => LoginViewModel(),
       child: findIdScreen(),
@@ -20,12 +21,71 @@ class FindIdScreen extends StatelessWidget {
 
   Widget findIdScreen() {
     return SafeArea(
-        child: Container(
-          alignment: Alignment.center,
-          child: Text(
-            "관리자 문의 요망"
+        child: Scaffold(
+          body: Container(
+            color: Colors.white,
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(),
+                Text(
+                  "관리자 문의 요망"
+                ),
+                routeButtonWidget()
+              ],
+            ),
           ),
         )
+    );
+  }
+
+  /// route Button
+  Widget routeButtonWidget() {
+    return Container(
+      width: 390.w,
+      height: 60.w,
+      alignment: Alignment.centerLeft,
+      padding: EdgeInsets.symmetric(horizontal: 25.w),
+      child: InkWell(
+        onTap: (){
+          FocusScope.of(_context).unfocus();
+          Navigator.pop(_context);
+        },
+        child: Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(28.w),
+              border: Border.all(
+                width: 2.w,
+                color: Color(0xFF050505),
+              )
+          ),
+          width: 90.w,
+          height: 48.w,
+          alignment: Alignment.centerLeft,
+          padding: EdgeInsets.only(left: 10.w),
+          child: Container(
+            width: 60.w,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(Icons.chevron_left_outlined, size: 30.w, color: Color(0xFF050505),),
+                Text(
+                  "이전",
+                  style: TextStyle(
+                      fontSize: 16.w,
+                      fontWeight: FontWeight.w600,
+                      color: Color(0xFF050505)
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
