722d2b10539b404515774d4c79169240387a983f
GeonhyoPark815 <rjsgy0815@gmail.com>
2022-06-16 23:51:28 +0900

    Fix: Fix text scale factor to 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,99 @@
+import 'package:amplify_api/amplify_api.dart';
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:couple_seflie_app/photly_style.dart';
 import 'package:couple_seflie_app/ui/splash_screen.dart';
-import 'package:couple_seflie_app/ui/view/screen/large_image_screen.dart';
-import 'package:couple_seflie_app/ui/view/screen/login/find_id_screen.dart';
-import 'package:couple_seflie_app/ui/view/screen/login/find_pw_screen.dart';
 import 'package:couple_seflie_app/ui/view/screen/login/login_screen.dart';
-import 'package:couple_seflie_app/ui/view/screen/post/post_detail_screen.dart';
+import 'package:couple_seflie_app/ui/view/screen/manage/manage_account_screen.dart';
 import 'package:couple_seflie_app/ui/view/screen/post/post_edit_screen.dart';
 import 'package:couple_seflie_app/ui/view/screen/post/post_main_screen.dart';
-import 'package:couple_seflie_app/ui/view/screen/register1/register_screen.dart';
-import 'package:couple_seflie_app/ui/view/screen/register2/register_anniversary_screen.dart';
-import 'package:couple_seflie_app/ui/view/screen/register2/register_username_screen.dart';
 import 'package:couple_seflie_app/ui/view_model/daily_couple_post_view_model.dart';
 import 'package:couple_seflie_app/ui/view_model/post_view_model.dart';
-//import 'package:couple_seflie_app/ui/view_model/register2_view_model.dart';
-//import 'package:couple_seflie_app/ui/view_model/register3_view_model.dart';
 import 'package:couple_seflie_app/ui/view_model/user_info_view_model.dart';
 import 'package:couple_seflie_app/ui/view_model/user_profile_view_model.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:overlay_support/overlay_support.dart';
 import 'package:provider/provider.dart';
 import 'amplifyconfiguration.dart';
 import 'data/repository/auth_service.dart';
+import 'data/repository/firebase_cloud_messaging_service.dart';
 import 'data/repository/local_notification_service.dart';
 
 bool _isLogined = false;
 late String username;
 
-main() async {
-  FlutterNativeSplash.preserve(widgetsBinding: WidgetsFlutterBinding.ensureInitialized());
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-  await ScreenUtil.ensureScreenSize();
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  print("백그라운드 실행");
+  print('Got a message whilst in the background!');
+  print('Message data: ${message.data.keys}');
+  print('Message data: ${message.data.values}');
+  const AndroidNotificationChannel channel = AndroidNotificationChannel(
+    'high_importance_channel',
+    'High Importance Notifications',
+    description: 'This channel is used for important notifications.',
+    importance: Importance.max,
+  );
 
-  // Add the Amplify category plugins, plugins should
-  // be added here in order for configuration to work
-  // DO NOT add plugins that you haven't configured via
-  // the Amplify CLI. This will throw a configuration error.
-  Amplify.addPlugins([AmplifyAuthCognito()]);
+  // background 푸시 알림 표시를 위한 local notifications 설정
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+  FlutterLocalNotificationsPlugin();
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+      AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
+  await flutterLocalNotificationsPlugin.initialize(
+      InitializationSettings(
+          android: AndroidInitializationSettings('@mipmap/ic_launcher'),
+          iOS: IOSInitializationSettings(
+            requestSoundPermission: true,
+            requestBadgePermission: true,
+            requestAlertPermission: true,
+          )),
+      onSelectNotification: (String? payload) async {});
+
+  // background 푸시 알림 핸들링
+  print('Got a message whilst in the background!');
+  print('Message data: ${message.data.keys}');
+  print('Message data: ${message.data.values}');
+  print("${message.data['pinpoint.notification.title']}");
+  print("${message.data['pinpoint.notification.body']}");
+  flutterLocalNotificationsPlugin.show(
+    0,
+    "${message.data['pinpoint.notification.title']}",
+    "${message.data['pinpoint.notification.body']}",
+    NotificationDetails(
+      android: AndroidNotificationDetails(
+        channel.id,
+        channel.name,
+        importance: Importance.max,
+        priority: Priority.max,
+        enableLights: true,
+        visibility: NotificationVisibility.public,
+      ),
+      iOS: IOSNotificationDetails(),
+    ),
+  );
+}
+
+main() async {
+  // Show Flutter native Splash with Bind Flutter Widget
+  FlutterNativeSplash.preserve(widgetsBinding: WidgetsFlutterBinding.ensureInitialized());
+  // Init Firebase to use FCM
+  //await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  // Init ScreenUtil to correspond to various device size
+  await ScreenUtil.ensureScreenSize();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  // Init Local Notification
+  LocalNotificationService().initialize();
+  // Add Amplify category plugins
+  Amplify.addPlugins([AmplifyAuthCognito(),AmplifyAPI(), ]);
 
   // Configure Amplify categories via the amplifyconfiguration.dart
   // configuration that was generated via the Amplify CLI
@@ -58,6 +112,7 @@ main() async {
     print('Could not configure Amplify ☠️');
   }
 
+  // Check Whether User Already Sign In
   final AuthFlowStatus authFlowStatus = await AuthService().checkAuthStatusService();
   if(authFlowStatus == AuthFlowStatus.session) {
     _isLogined = true;
@@ -71,8 +126,6 @@ main() async {
         ChangeNotifierProvider(create: (context)=> DailyCouplePostViewModel(),),
         ChangeNotifierProvider(create: (context)=> PostViewModel()),
         ChangeNotifierProvider(create: (context) => UserInfoViewModel()),
-        //ChangeNotifierProvider(create: (context) => Register2ViewModel()),
-        //ChangeNotifierProvider(create: (context) => Register3ViewModel()),
         ChangeNotifierProvider(create: (context) => UserProfileViewModel()),
       ],
       child: MyApp())
@@ -81,48 +134,47 @@ main() async {
 
 class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
-  late DailyCouplePostViewModel _dailyCouplePostViewModel;
-  late UserProfileViewModel _userProfileViewModel;
+  bool _isLoading = false;
+
   @override
   Widget build(BuildContext context) {
-    _dailyCouplePostViewModel = Provider.of<DailyCouplePostViewModel>(context);
-    _userProfileViewModel = Provider.of<UserProfileViewModel>(context, listen: false);
     FlutterNativeSplash.remove();
-    if (_isLogined) {
-      _userProfileViewModel.setCurrentUser();
-      _dailyCouplePostViewModel.initDailyCouplePosts();
-      LocalNotificationService().initialize();
+    if (_isLogined && !_isLoading) {
+      _isLoading = true;
+      Provider.of<UserProfileViewModel>(context, listen: false).setCurrentUser();
+      Provider.of<DailyCouplePostViewModel>(context,listen: false).initDailyCouplePosts();
+      FirebaseCloudMessagingService().fcmSetting();
     }
 
+    print("myApp 실행");
     return ScreenUtilInit(
         splitScreenMode: false,
-        //designSize: const Size(390, 840),
         designSize: const Size(375, 667),
-        builder: (context, _) => MaterialApp(
-              debugShowCheckedModeBanner: false,
-              title: 'Photly',
-              theme: PhotlyStyle(MediaQueryData()).photlyThemeData,
-              home: _isLogined ?
-              _dailyCouplePostViewModel.dailyCouplePosts.isEmpty || _dailyCouplePostViewModel.loading ?
-              // Show loading widget when is loading
-              SplashScreen()
-                  :
-              PostMainScreen()
-                  :
-              LoginScreen(),
-              routes: {
-                '/postMainScreen': (context) => PostMainScreen(),
-                '/postEditScreen': (context) => PostEditScreen(),
-                '/postDetailScreen': (context) => PostDetailScreen(),
-                '/largeImageScreen': (context) => LargeImageScreen(),
-                '/loginScreen': (context) => LoginScreen(),
-                '/findIdScreen': (context) => FindIdScreen(),
-                '/findPwScreen': (context) => FindPwScreen(),
-                '/registerScreen': (context) => RegisterScreen(),
-                '/registerUsernameScreen': (context) => RegisterUsernameScreen(),
-                '/registerAnniversaryScreen': (context) => RegisterAnniversaryScreen(),
-              },
-            )
+        builder: (context, _) => OverlaySupport(
+          child: MaterialApp(
+            builder: (context, child) => MediaQuery(
+              data: MediaQuery.of(context).copyWith(
+                textScaleFactor: 1.0,
+              ),
+              child: child!,
+            ),
+            debugShowCheckedModeBanner: false,
+            title: 'Photly',
+            theme: PhotlyStyle(MediaQueryData()).photlyThemeData,
+            home: _isLogined ?
+            Provider.of<DailyCouplePostViewModel>(context).isLoadDone ?
+            // Show loading widget when is loading
+            PostMainScreen()
+                :
+            SplashScreen()
+                :
+            LoginScreen(),
+            routes: {
+              '/postEditScreen': (context) => PostEditScreen(),
+              '/manageAccountScreen': (context) => ManageAccountScreen(),
+            },
+          ),
+        )
     );
   }
 }
