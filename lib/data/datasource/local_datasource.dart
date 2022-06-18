36aff356e12b3ff97e7de450a964201a8cd82c82
GeonhyoPark815 <rjsgy0815@gmail.com>
2022-06-18 14:18:55 +0900

    Fix: Change camera from rear to front

--- lib/data/datasource/local_datasource.dart
+++ lib/data/datasource/local_datasource.dart
@@ -5,7 +5,7 @@ import 'package:image_picker/image_picker.dart';
 class LocalDataSource {
   // get image through imagePicker
   Future<File?> getImage(ImageSource imageSource) async {
-    final image = await ImagePicker().pickImage(source: imageSource);
+    final image = await ImagePicker().pickImage(source: imageSource, preferredCameraDevice: CameraDevice.front,);
     if(image != null){
       return File(image.path);
     }
